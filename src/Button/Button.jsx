5f8cbb1a94327c65a75469602ee85803fce251ef
Newton Lomar <newtonlomar16@gmail.com>
2022-08-01 08:58:02 -0300

    Hero Buttons done

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -1,13 +1,24 @@
+import emailLogo from '../assets/email.png';
+import linkedinLogo from '../assets/linkedin.png';
+
 export default function Button({ type }) {
   return type === 'email' ? (
-    <a className='button__email'>
-      <img src={emailLogo} alt='email logo' />
-      <h5>Email</h5>
+    <a
+      className='button__email'
+      href='mailto:newtonlomar16@gmail.com'
+      target='_blank'
+    >
+      <img className='emailLogo' src={emailLogo} alt='email logo' />
+      <h5 className='h5'>Email</h5>
     </a>
   ) : (
-    <a className='button__linkedin'>
-      <img src={linkedinLogo} alt='email logo' />
-      <h5>Linkedin</h5>
+    <a
+      className='button__linkedin'
+      href='https://www.linkedin.com/in/newtonlomar/'
+      target='_blank'
+    >
+      <img className='linkedinLogo' src={linkedinLogo} alt='email logo' />
+      <h5 className='h5'>LinkedIn</h5>
     </a>
   );
 }
--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import profile from '../assets/profile.jpg';
-
+import Button from '../Button/Button';
 export default function Hero() {
   return (
     <div>
@@ -12,8 +12,14 @@ export default function Hero() {
         <h1 className='h1'>Newton Lomar</h1>
         <h2 className='h2'>Frontend Developer</h2>
         <h3 className='h3'>
-          <a href='https://www.newtonlomar.com'>newtonlomar.com</a>
+          <a href='https://www.newtonlomar.com' target='_blank'>
+            newtonlomar.com
+          </a>
         </h3>
+      </div>
+      <div className='hero__buttons'>
+        <Button type='email' />
+        <Button type='linkedin' />
       </div>
     </div>
   );
