a9af1757b4a5283dcdadfef4c466305c84f0d9a1
Newton Lomar <newtonlomar16@gmail.com>
2022-07-31 23:02:03 -0300

    added and styled image, name, role and website to Hero component

--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -1,7 +1,20 @@
+import profile from '../assets/profile.jpg';
+
 export default function Hero() {
   return (
     <div>
-      <h1>Hero</h1>
+      <img
+        className='profile_pic'
+        src={profile}
+        alt="Newton Lomar's Profile Pic"
+      />
+      <div className='hero__text'>
+        <h1 className='h1'>Newton Lomar</h1>
+        <h2 className='h2'>Frontend Developer</h2>
+        <h3 className='h3'>
+          <a href='https://www.newtonlomar.com'>newtonlomar.com</a>
+        </h3>
+      </div>
     </div>
   );
 }
