b0aaba633fb1a4f43688cea1f1e56b48d62a1d00
Newton Lomar <newtonlomar16@gmail.com>
2022-08-01 23:53:43 -0300

    social component done

--- src/Social/Social.jsx
+++ src/Social/Social.jsx
@@ -1,7 +1,23 @@
+import twitter from '../assets/twitter.png';
+import facebook from '../assets/facebook.png';
+import instagram from '../assets/instagram.png';
+import github from '../assets/github.png';
+
 export default function Social() {
   return (
-    <div>
-      <h1>Social</h1>
-    </div>
+    <footer className='footer'>
+      <a href='https://twitter.com/lomar_newton' target='_blank'>
+        <img src={twitter} alt='twitter logo' />
+      </a>
+      <a href='https://www.youtube.com/watch?v=a3Z7zEc7AXQ' target='_blank'>
+        <img src={facebook} alt='facebook logo' />
+      </a>
+      <a href='https://www.instagram.com/feitosanewton/' target='_blank'>
+        <img src={instagram} alt='instagram logo' />
+      </a>
+      <a href='https://github.com/newlomar' target='_blank'>
+        <img src={github} alt='github logo' />
+      </a>
+    </footer>
   );
 }
