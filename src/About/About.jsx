ba841567dbb0dbd412ce85b47873b6423b2316af
Newton Lomar <newtonlomar16@gmail.com>
2022-08-01 22:53:43 -0300

    about component structure developed

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,7 +1,23 @@
 export default function About() {
   return (
     <div>
-      <h1>About</h1>
+      <section className='about about__about'>
+        <h4>About</h4>
+        <p>
+          I am a frontend developer with a particular interest in making things
+          simple and automating daily tasks. I try to keep up with security and
+          best practices, and am always looking for new things to learn. I
+          choiced js as my primary langugage and I try to solve problems using
+          it, but I'm definitely technology agnostic.
+        </p>
+      </section>
+      <section className='about about__interests'>
+        <h4>Interests</h4>
+        <p>
+          League Of Legends, casual programming stuff, solving problems, helping
+          people, spend time with family and friends.
+        </p>
+      </section>
     </div>
   );
 }
