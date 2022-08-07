13a16ec1a66733650b2ba0ff56d1b9812e66c350
Newton Lomar <newtonlomar16@gmail.com>
2022-08-07 11:38:23 -0300

    color adjusts

--- src/TypeCardButtons/TypeCardButtons.jsx
+++ src/TypeCardButtons/TypeCardButtons.jsx
@@ -1,10 +1,18 @@
 export default function TypeCardButtons() {
   return (
     <section>
-      <input type='checkbox' id='switchThemeColor' />
-      <label htmlFor='switchThemeColor'>Toggle</label>
-      <input type='checkbox' id='switchDesign' />
-      <label htmlFor='switchDesign'>Toggle</label>
+      <div>
+        <input type='checkbox' id='switchThemeColor' />
+        <label htmlFor='switchThemeColor' className='switchTheme'>
+          Toggle
+        </label>
+      </div>
+      <div>
+        <input type='checkbox' id='switchDesign' />
+        <label htmlFor='switchDesign' className='switchTheme design'>
+          Toggle
+        </label>
+      </div>
     </section>
   );
 }
