49c2512b04095e2ff6e5e985a652de11f8960daa
Newton Lomar <newtonlomar16@gmail.com>
2022-08-07 11:10:52 -0300

    working on switch component

---
+++ src/TypeCardButtons/TypeCardButtons.jsx
@@ -0,0 +1,10 @@
+export default function TypeCardButtons() {
+  return (
+    <section>
+      <input type='checkbox' id='switchThemeColor' />
+      <label htmlFor='switchThemeColor'>Toggle</label>
+      <input type='checkbox' id='switchDesign' />
+      <label htmlFor='switchDesign'>Toggle</label>
+    </section>
+  );
+}
